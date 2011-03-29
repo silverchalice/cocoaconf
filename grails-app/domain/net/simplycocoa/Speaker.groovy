c5fcf32099027ce346f80f53c98d31e5d6fef216
Benjamin Klein <ben@silver-chalice.com>
2011-03-29 17:15:04 -0500

    add Speaker domain class

---
+++ grails-app/domain/net/simplycocoa/Speaker.groovy
@@ -0,0 +1,18 @@
+package net.simplycocoa
+
+class Speaker {
+	
+	String firstName
+	String lastName
+	String email
+	String bio
+	String imagePath
+
+    static constraints = {
+	    firstName nullable:false
+	    lastName nullable:false
+	    email nullable:false, email:true
+	    bio nullable:true, maxSize: 2000
+	    imagePath nullable:true
+    }
+}
