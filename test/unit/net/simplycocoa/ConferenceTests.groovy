5193b180fa3d27ff21d3e612af30697ffa057496
Benjamin Klein <ben@silver-chalice.com>
2011-03-30 12:24:33 -0500

    add Conference class

---
+++ test/unit/net/simplycocoa/ConferenceTests.groovy
@@ -0,0 +1,17 @@
+package net.simplycocoa
+
+import grails.test.*
+
+class ConferenceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
