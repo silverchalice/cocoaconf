26398e4c49bcfc179f8b24a037735407c13fcb20
Benjamin Klein <ben@silver-chalice.com>
2011-03-30 12:48:58 -0500

    add Venue class

---
+++ test/unit/net/simplycocoa/VenueTests.groovy
@@ -0,0 +1,17 @@
+package net.simplycocoa
+
+import grails.test.*
+
+class VenueTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
