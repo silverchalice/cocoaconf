7ecd3560765ca2a6e19e7b1b8a24af5a393e858f
Benjamin Klein <ben@silver-chalice.com>
2011-04-06 15:22:56 -0500

    give adminRole to admin (Bootstrap.groovy)

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -11,6 +11,7 @@ class BootStrap {
 		def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
 	    if(!User.findByUsername("admin")){
 		    def admin = new User(username:"admin", password:springSecurityService.encodePassword("admin"), enabled:true, accountExpired:false, accountLocked:false, passwordExpired:false).save(failOnError:true)
+		    UserRole.create admin, adminRole
 		}
     }
     def destroy = {
