58236fc53868400fe0659f0b54b1e4f69ba4af1b
Benjamin Klein <ben@silver-chalice.com>
2011-04-25 17:27:07 -0500

    Session now belongsTo Conference (and of course should have all along)

--- grails-app/domain/com/cocoaconf/Session.groovy
+++ grails-app/domain/com/cocoaconf/Session.groovy
@@ -3,12 +3,15 @@ package com.cocoaconf
 
 class Session {
 
+    Conference conference
     Presentation presentation
     Date startTime
     Date endTime
     String room
     Speaker speaker
     Integer track
+
+    static belongsTo = Conference
 
     static constraints = {
 	    startTime nullable:true
