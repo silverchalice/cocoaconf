5792120418d57d5ed21b539f03f041a297459f27
Benjamin Klein <ben@silver-chalice.com>
2011-04-26 15:13:41 -0500

    add Registration class

---
+++ grails-app/domain/com/cocoaconf/Registration.groovy
@@ -0,0 +1,13 @@
+package com.cocoaconf
+
+class Registration {
+
+    Conference conference
+    User user
+    Date dateCreated
+    Double fee
+    String comments
+	
+    static constraints = {
+    }
+}
