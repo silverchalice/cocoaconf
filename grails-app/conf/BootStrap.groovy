d9e8ee47d318f78ce6c111e87738d327b22282e9
Benjamin Klein <ben@silver-chalice.com>
2011-04-26 15:13:07 -0500

    add fields to admin user create in BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,7 +10,7 @@ class BootStrap {
 	    def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
 		def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
 	    if(!User.findByUsername("admin")){
-		    def admin = new User(username:"admin", password:springSecurityService.encodePassword("admin"), enabled:true, accountExpired:false, accountLocked:false, passwordExpired:false).save(failOnError:true)
+		    def admin = new User(username:"admin", password:springSecurityService.encodePassword("admin"), email: "admin@cocoaconf.com", firstName: "Admin", lastName: "Cocoaconf", street: "123 Main Street", city: "City", state: "ST", zip:"53901", enabled:true, accountExpired:false, accountLocked:false, passwordExpired:false).save(failOnError:true)
 		    UserRole.create admin, adminRole
 		}
     }
