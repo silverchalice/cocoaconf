c4fe352f3b809cac101833f406587408e3bf2e41
Benjamin Klein <ben@silver-chalice.com>
2011-04-27 15:56:31 -0500

    set venue's imagePath in BootStrap // i am such an idiot

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -17,7 +17,7 @@ class BootStrap {
             String ed = "2012/10/22"
             SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
 		
-		    def venue =  new Venue(description:"This is the great venue that you should hold a conference at.", phone:"(123) 456-7890", website:"http://venue.com/", image:new File("/Users/ebenezer/Dropbox/inkleinations/DSCN9493.JPG").readBytes(), address:"123 Main Street", city:"Madison", name:"The Great Venue", state:"WI", zip:"53901").save(failOnError:true)
+		    def venue =  new Venue(description:"This is the great venue that you should hold a conference at.", phone:"(123) 456-7890", website:"http://venue.com/", imagePath:"images/venue/the-great-venue/DSCN9493.JPG", address:"123 Main Street", city:"Madison", name:"The Great Venue", state:"WI", zip:"53901").save(failOnError:true)
 		
 		    def conference = new Conference(startDate:formatter.parse(sd), endDate:formatter.parse(ed), venue: Venue.get(1), description:"This will be the great conference that you should go to.", city:"Madison", state:"WI", tinyName:"madison2012").save(failOnError:true)
 		
