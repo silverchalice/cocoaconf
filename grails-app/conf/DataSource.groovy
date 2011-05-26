9f80e230cc0b127e3613fdec9195b00e52c70bf3
Zachary Klein <zak@silver-chalice.com>
2011-05-26 15:08:02 -0500

    Check env before adding BootStrap data; added keep-alive props to Datasource.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -32,6 +32,11 @@ environments {
 			dbCreate = "update"
             url = "jdbc:mysql://127.0.0.1:3306/cocoaDb"
 			dialect=org.hibernate.dialect.MySQLInnoDBDialect
+            properties {
+				validationQuery="select 1"
+				testWhileIdle=true
+				timeBetweenEvictionRunsMillis=60000
+			}
 
         }
     }
