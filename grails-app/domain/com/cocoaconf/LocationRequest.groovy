7e6fe63a4cfb01336b66b7dd91a7eee6a088dfd5
Benjamin Klein <ben@silver-chalice.com>
2011-07-29 09:50:46 -0500

    we may as well requequire name + email

--- grails-app/domain/com/cocoaconf/LocationRequest.groovy
+++ grails-app/domain/com/cocoaconf/LocationRequest.groovy
@@ -8,8 +8,7 @@ class LocationRequest {
     String state
 
     static constraints = {
-        email email:true, nullable:true, blank:true
-        name  nullable:true, blank:true
+        email email:true, nullable:false, blank:false
         city nullable: false, blank:false
         state nullable:false, blank:false; inList: ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
