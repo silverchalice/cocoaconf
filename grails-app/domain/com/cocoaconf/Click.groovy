234fb0290f87dab7d118422def2a71ff0a21fb68
David Klein <davidklein@Davids-MacBook-Pro.local>
2013-11-30 14:50:11 -0600

    Fixing Click class.

--- grails-app/domain/com/cocoaconf/Click.groovy
+++ grails-app/domain/com/cocoaconf/Click.groovy
@@ -1,7 +1,14 @@
 package com.cocoaconf
 
 class Click {
+	Date dateCreated
+	String referer
+	String tag
+	Conference conference
 
     static constraints = {
+    	referer nullable:true
+    	tag nullable:true
+    	conference nullable:true
     }
 }
