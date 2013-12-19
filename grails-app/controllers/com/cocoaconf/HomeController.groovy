2a01637668a6500b7410e6bd92488cd5c37b9fdb
Benjamin Klein <ben@silver-chalice.com>
2013-12-19 14:34:23 -0600

    update name of file /sponsor redirects to

--- grails-app/controllers/com/cocoaconf/HomeController.groovy
+++ grails-app/controllers/com/cocoaconf/HomeController.groovy
@@ -108,7 +108,7 @@ class HomeController {
     }
 
     def prospectus = {
-      redirect(uri: resource(dir: 'files', file: 'sponsor.pdf'))
+      redirect(uri: resource(dir: 'files', file: 'CocoaConf-Sponsorship-Prospectus-Spring-2014.pdf'))
       return
     }
 
