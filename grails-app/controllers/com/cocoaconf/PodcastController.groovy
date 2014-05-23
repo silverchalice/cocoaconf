246a749514602cb549589f8b7d3f3fd34dd72c00
Benjamin Klein <ben@silver-chalice.com>
2014-05-23 12:06:06 -0500

    make PodcastController 'index' action work with pagination

--- grails-app/controllers/com/cocoaconf/PodcastController.groovy
+++ grails-app/controllers/com/cocoaconf/PodcastController.groovy
@@ -3,12 +3,17 @@ package com.cocoaconf
 class PodcastController {
 
     def index() {
+      params.with {
+        max = Math.min(params?.max ? params?.int('max') : 5, 100)
+        sort = 'episodeNumber'
+        order = 'desc'
+      }
     	def episodes = []
     	if (params.id){
     	  episodes << Episode.findByEpisodeNumber(params.id)
     	} else {
-    		episodes = Episode.list(sort:'episodeNumber', order: 'desc')
+    		episodes = Episode.list(params)
     	}
-    	[episodes:episodes]
+    	[episodes:episodes, episodeInstanceTotal: Episode.count()]
     }
 }
