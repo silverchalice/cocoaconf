a7482d1fedf2afbc068ad183ac482dfa42d8b93e
David Klein <davidklein@Davids-MacBook-Pro.local>
2014-12-01 00:31:24 -0600

    Adding some iamges and a minor fix to conference/home.gsp

--- grails-app/controllers/com/cocoaconf/HomeController.groovy
+++ grails-app/controllers/com/cocoaconf/HomeController.groovy
@@ -120,7 +120,7 @@ class HomeController {
     }
 
     def prospectus = {
-      redirect(uri: resource(dir: 'files', file: 'CocoaConf_Fall 2014_Sponsorship_Brochure.pdf'))
+      redirect(uri: resource(dir: 'files', file: 'Sponsorship-Spring-2015.pdf'))
       return
     }
 
